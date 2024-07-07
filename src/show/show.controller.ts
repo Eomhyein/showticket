8b965bb763f53f149f194bf103f7cb5b944d66e1
hyein <mickey_8688@naver.com>
2024-07-08 02:37:21 +0900

    #6 : show error, #7, #8, #9 : show error

--- src/show/show.controller.ts
+++ src/show/show.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { RolesGuard } from 'src/auth/Roles/roles.guard';
+import { ShowService } from './show.service';
+import { Roles } from 'src/auth/Roles/roles.decorator';
+import { Role } from 'src/user/types/userRole.type';
+import { CreatedShowDto } from './dto/created-show.dto';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('show')
-export class ShowController {}
+@Controller('shows')
+export class ShowController {
+  constructor(private readonly showService: ShowService) {}
+
+  // 공연 등록 (ADMIN만 등록가능)
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(RolesGuard)
+  @Roles(Role.Admin)
+  async createdShow(@Body() createdShowDto: CreatedShowDto) {
+    return await this.showService.createdShow(createdShowDto);
+  }
+
+  // 공연 조회
+  @Get()
+  async findAll() {
+    return await this.showService.findAll();
+  }
+
+  // 카테고리로 조회
+  // @Get('category')
+  // async findByCategory(@Query('category') category: Category) {
+  //   return this.showService.findByCategory(category);
+  // }
+  // 공연 상세 조회
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard('jwt'))
+  @Get(':showId')
+  async findById(@Param('showId') showId: number) {
+    return await this.showService.findById(showId);
+  }
+
+  // 공연 검색
+  @Get('Search')
+  async searchByShowName(@Query('showName') showName: string) {
+    return await this.showService.searchByShowName(showName);
+  }
+}
--- src/show/show.service.ts
+++ src/show/show.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { Show } from './entities/show.entity';
+import { Repository } from 'typeorm';
+import { CreatedShowDto } from './dto/created-show.dto';
 
 @Injectable()
-export class ShowService {}
+export class ShowService {
+  constructor(
+    @InjectRepository(Show)
+    private showRepository: Repository<Show>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  // 새 공연 생성
+  async createdShow(createdShowDto: CreatedShowDto): Promise<Show> {
+    const {
+      showName,
+      description,
+      category,
+      location,
+      price,
+      image_url,
+      show_datetime,
+      show_runtime,
+      total_seats,
+    } = createdShowDto;
+    const newShow = this.showRepository.create({
+      showName,
+      description,
+      category,
+      location,
+      price,
+      image_url,
+      show_datetime,
+      show_runtime,
+      total_seats,
+    });
+    return await this.showRepository.save(newShow);
+  }
+
+  // 공연 전체 조회
+  async findAll(): Promise<Show[]> {
+    return await this.showRepository.find();
+  }
+  // // 공연 카테고리로 조회
+  // async findByCategory(category: string): Promise<Show> {
+  //   return await this.showRepository.find({ where: { category } });
+  // }
+
+  // 공연 상세 조회
+  async findById(showId: number): Promise<Show> {
+    return await this.showRepository.findOne({ where: { showId } });
+  }
+  // 공연 검색
+  async searchByShowName(showName: string): Promise<Show[]> {
+    return await this.showRepository.find({ where: { showName } });
+  }
+}
